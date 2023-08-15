06f942caa86d4dd5fb7dab44be08b840e37b65d4
Nong AnuchitO <anuchit.prasertsang@gmail.com>
2023-08-15 15:20:38 +0700

    add

--- src/taxi.test.ts
+++ src/taxi.test.ts
@@ -1,12 +1,51 @@
+import {Taxi, calculateFare, roundDistance} from './taxi'
 
-import { Taxi } from "./taxi"
+describe('Taxi', () => {
+  describe('calculateFare', () => {
+    it('should return total of 1km without waiting time', () => {
+      const distance = 1
+      const waitingTime = 0
 
-describe("Taxi", () => {
+      const result = calculateFare(distance, waitingTime)
 
-	it("example test", () => {
-	   const result = Taxi()
-	   expect(result).toEqual(55)
-	})
+      expect(result).toEqual(2)
+    })
 
+    it('should return total of 2km without waiting time', () => {
+      const distance = 2
+      const waitingTime = 0
+
+      const result = calculateFare(distance, waitingTime)
+
+      expect(result).toEqual(4)
+    })
+
+    it('should return total of 0km with 1m waiting time', () => {
+      const distance = 0
+      const waitingTime = 1
+
+      const result = calculateFare(distance, waitingTime)
+
+      expect(result).toEqual(1)
+    })
+
+    it('should return total of 2km with 2m waiting time', () => {
+      const distance = 2
+      const waitingTime = 2
+
+      const result = calculateFare(distance, waitingTime)
+
+      expect(result).toEqual(6)
+    })
+  })
+
+  describe('rounding distance', () => {
+    it('should round up distance to 0.5km', () => {
+      const distance = 0.1
+
+      const result = roundDistance(distance)
+
+      expect(result).toEqual(0.5)
+    })
+  })
 })
-
--- src/taxi.ts
+++ src/taxi.ts
@@ -1,2 +1,17 @@
-
 export const Taxi = () => 55
+
+export function calculateFare(distance, waitingTime) {
+  return 2 * distance + waitingTime
+}
+
+export const roundDistance = (distance: number): number => {
+  return Math.ceil(distance * 2) / 2
+}
+
+export const roundWaitingTime = (waitingTime: number): number => {
+  return Math.ceil(waitingTime)
+}
+
+export const fare = (distance: number, waitingTime: number): number => {
+  return calculateFare(roundDistance(distance), roundWaitingTime(waitingTime))
+}
